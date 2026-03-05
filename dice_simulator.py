ac5181323c9ab1fc7b87d17d3098748b54303b66
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-05 05:29:13 -0800

    Update and rename

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,10 +1,13 @@
-#Dice rolling simulator:
+#python based Dice rolling simulator:
+#importing random module
 import random as rd
-
+#while loop running
 while True:
     print("dice:", rd.randint(1,6))
+    #conditional statements
     if(input("roll again? (y/n):") != 'y'):
         break
+
 
 
 
